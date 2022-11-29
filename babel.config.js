61c952a28da69b52ee410e300c1ced3500d96b32
oakis <oakismen@gmail.com>
2022-11-29 11:03:46 +0100

    Update module configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          components: './src/components',
+          views: './src/views',
+          utils: './src/utils',
+          contexts: './src/contexts',
+          hooks: './src/hooks',
+          src: './src',
+        },
+      },
+    ],
+  ],
 };
